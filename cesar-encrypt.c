37b81988c50df999c35afae5b717bf142d57e464
Gökhan TOPUZ <gokhan061@msn.com>
2024-06-25 22:39:21 +0300

    Update cesar-encrypt.c

--- cesar-encrypt.c
+++ cesar-encrypt.c
@@ -1,43 +1,48 @@
 #include<stdio.h>
 
-int main(){
+int main()
+{
+    char message[100], ch;
+    int i, key;
 
-	char message[100], ch;
-	int i, key;
+    printf("Enter a message to encrypt: ");
+    gets(message);
 
-	printf("Enter a message to encrypt: ");
-	gets(message);
+    printf("Enter key: ");
+    scanf("%d", &key);
 
-	printf("Enter key: ");
-	scanf("%d", &key);
+    for(i = 0; message[i] != '\0'; ++i)
+    {
+        ch = message[i];
 
-	for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
+        if(ch >= 'a' && ch <= 'z')
+        {
+            ch = ch + key;
 
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch + key;
+            if(ch > 'z')
+            {
+                ch = ch - 'z' + 'a' - 1;
+            }
 
-			if(ch > 'z'){
-				ch = ch - 'z' + 'a' - 1;
-			}
+            message[i] = ch;
+        }
 
-			message[i] = ch;
-		}
+        else if(ch >= 'A' && ch <= 'Z')
+        {
+            ch = ch + key;
 
-		else if(ch >= 'A' && ch <= 'Z'){
-			ch = ch + key;
+            if(ch > 'Z')
+            {
+                ch = ch - 'Z' + 'A' - 1;
+            }
 
-			if(ch > 'Z'){
-				ch = ch - 'Z' + 'A' - 1;
-			}
+            message[i] = ch;
+        }
+    }
 
-			message[i] = ch;
-		}
-	}
-
-	printf("Encrypted message: %s", message);
+    printf("Encrypted message: %s", message);
 
 
-	return 0;
+    return 0;
 }
 
